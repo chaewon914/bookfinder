cf335e36881159aae5636633bb7a177732afeba4
choiminjeong <61730105+choiminjeong@users.noreply.github.com>
2020-07-07 17:03:17 +0900

    Update app.py
    search.html 경로 생성

--- app.py
+++ app.py
@@ -5,23 +5,31 @@ from bs4 import BeautifulSoup
 
 from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+db = client.bookfinder                     # 'dbsparta'라는 이름의 db를 만듭니다.
 
 ##########################
 ###### HTML주는 부분######
 ##########################
 
+## 메인HTML을 주는 부분
 @app.route('/')
 def home():
    return render_template('index.html')
 
+## 대여HTML을 주는 부분
 @app.route('/rent')
 def rent():
    return render_template('rent.html')
 
+## 구매HTML을 주는 부분
 @app.route('/buy')
 def buy():
    return render_template('buy.html')
+
+## 검색HTML을 주는 부분
+@app.route('/search')
+def search():
+   return render_template('search.html')
 
 ##########################
 ###### 주는 부분######
